3cca5cf8b58265196785ba665c8c3f31d8ce0691
Кульбида Владимир Александрович <V.Kulbida@omsk.otpbank.ru>
2021-05-21 12:11:05 +0600

    optimization TF file ver3

--- playbook/main.tf
+++ playbook/main.tf
@@ -120,15 +120,14 @@ output "production_public_ip" {
     value = google_compute_instance.terraform-production.network_interface[0].access_config[0].nat_ip
 }
 
-/*resource "time_sleep" "wait_30_seconds" {
+resource "time_sleep" "wait_30_seconds" {
   depends_on = [google_compute_instance.terraform-production, google_compute_instance.terraform-staging]
 
   create_duration = "30s" // Change to 90s
 }
-*/
+
 resource "null_resource" "ansible_hosts_provisioner" {
-//  depends_on = [time_sleep.wait_30_seconds]
-  depends_on = [google_compute_instance.terraform-production, google_compute_instance.terraform-staging]
+  depends_on = [time_sleep.wait_30_seconds]
   provisioner "local-exec" {
     interpreter = ["/bin/bash" ,"-c"]
     command = <<EOT
@@ -144,15 +143,14 @@ resource "null_resource" "ansible_hosts_provisioner" {
   }
 }
 
-/*resource "time_sleep" "wait_5_seconds" {
+resource "time_sleep" "wait_5_seconds" {
   depends_on = [null_resource.ansible_hosts_provisioner]
 
   create_duration = "5s"
 }
-*/
+
 resource "null_resource" "ansible_playbook_provisioner" {
-//  depends_on = [time_sleep.wait_5_seconds]
-  depends_on = [null_resource.ansible_hosts_provisioner]
+  depends_on = [time_sleep.wait_5_seconds]
   provisioner "local-exec" {
     command = "ansible-playbook -u root --vault-password-file 'vault_pass' --private-key '/root/.ssh/id_rsa' -i inventory/hosts main.yml"
   }
